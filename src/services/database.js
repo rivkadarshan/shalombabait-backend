4f0a9b8bd4945a8a9f700d95e4afe84735f9344d
s-greenva <3387sar@gmail.com>
2025-09-03 11:12:39 +0300

    fix add tabale users

--- src/services/database.js
+++ src/services/database.js
@@ -1,12 +1,13 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT || 3306,
   });
   console.log('USER:', process.env.MYSQL_USER);
   console.log('PASSWORD:', process.env.MYSQL_PASSWORD);
