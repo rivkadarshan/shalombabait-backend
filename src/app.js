9df2633da2d611de8b22e5c3614de0b218a0a154
Rivka Darshan <rivka3188@gmail.com>
2025-09-18 12:12:23 +0300

    add service to app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-import usersRouter from './modules/users/user.routes.js';
+// import usersRouter from './modules/users/user.routes.js';
 import emailRoutes from './modules/email/email.routes.js';
+import patientRoutes from './modules/patients/patient.routes.js'
+import appointmentsRoutes from './modules/appointments/appointment.routes.js'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', usersRouter);
+// app.use('/api/users', usersRouter);
 app.use('/api/email', emailRoutes);
+app.use('/api/patient', patientRoutes);
+app.use('/api/appointments', appointmentsRoutes);
+// app.use('/api/rooms', usersRouter);
+// app.use('/api/therapists', emailRoutes);
+// app.use('/api/treatmentTypes', emailRoutes);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
 
-// const express = require('express');
-// const app = express();
-// const cors = require('cors')
-// //טוען את הראוטרים מהתקיה
-// const routers = require('./routers/routers');
-
-// app.use(cors());
-// app.use(express.json());
-
-// //כל הראוטרים ירוכזו כאן
-// app.use('/', routers);
-
-// //מה זה?
-// app.get('/', (req, res) => {
-//   res.send('Welcome to server routers');
-// });
-
-// module.exports = app;
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+  });
